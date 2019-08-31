bd6d0cb010cbca35dbee6a3fd018fad21d5646c7
Hamza Zoumhani <zoumhanihamza@gmail.com>
2019-08-31 11:02:39 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ const example =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 function filter(array, callback){
+    const filtered=[]
     for(let i=0; i<a.length; i++){
-        
-
-        callback(array[i])
+        if(callback(array[i])){
+            filtered.push(array[i])
+        }
     }
 
-    return 
+    return filtered
 }
