f3a8813f37e7db8e13a234d5b32a51ddb563636d
Hamza Zoumhani <zoumhanihamza@gmail.com>
2019-08-31 10:57:38 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+const example =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+function filter(array, callback){
+    for(let i=0; i<a.length; i++){
+        
+
+        callback(array[i])
+    }
+
+    return 
+}
